d1ee240ef2cf306923f6f658510a2ef5387bb74b
CardosoB8 <166424120+CardosoB8@users.noreply.github.com>
2025-08-19 17:42:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.get('/post/:id', (req, res) => {
                 <meta charset="UTF-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
                 <title>${post.titulo}</title>
-                <link rel="stylesheet" href="/public/style.css">
+                <link rel="stylesheet" href="style.css">
             </head>
             <body>
                 <header>
